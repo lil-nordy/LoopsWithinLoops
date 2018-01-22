581758a044a89a98d56bb1a364ad222d7076e1b8
nordqunn <nordqunn@rose-hulman.edu>
2018-01-21 20:19:54 -0500

    quick line for emphasis

--- src/m2e_nested_loops_in_sequences.py
+++ src/m2e_nested_loops_in_sequences.py
@@ -111,6 +111,7 @@ def sequence_of_sequence_example2(sequence_of_lists):
     for j in range(len(sequence_of_lists)):
         for k in range(len(sequence_of_lists[j])):
             sequence_of_lists[j][k] = sequence_of_lists[j][k] * (j + 1)
+            # sequence_of_lists[j][k] *= (j + 1)
 
 
 # ----------------------------------------------------------------------
